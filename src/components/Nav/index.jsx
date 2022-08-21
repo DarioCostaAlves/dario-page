bf98d9de9caa7ce50a839d64be305e4565ef6a9d
DarioCostaAlves <dario.c.alves22@gmail.com>
2022-08-21 16:58:48 +0100

    building header&&nav

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,15 +1,14 @@
 import './styles.css';
 import {BiHomeCircle, BiBookOpen} from 'react-icons/bi';
-import {FcAbout} from 'react-icons/fc';
 import{GrContact} from 'react-icons/gr';
 import{RiServiceLine} from 'react-icons/ri';
-import{BsChatText} from 'react-icons/bs';
+import{BsChatText,BsInfoCircle} from 'react-icons/bs';
 
 export default function Nav() {
   return (
     <nav>
       <a href="#"><BiHomeCircle/></a>
-      <a href="#about"><FcAbout/></a>
+      <a href="#about"><BsInfoCircle/></a>
       <a href="#experience"><BiBookOpen/></a>
       <a href="#services"><RiServiceLine/></a>
       <a href="#contact"><BsChatText/></a>
