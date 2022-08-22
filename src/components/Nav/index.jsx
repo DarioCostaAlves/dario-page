9d3aef8b8028cb7226944451377a77d3ef52a8dc
DarioCostaAlves <dario.c.alves22@gmail.com>
2022-08-22 15:43:24 +0100

    active nav

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -3,15 +3,42 @@ import {BiHomeCircle, BiBookOpen} from 'react-icons/bi';
 import{GrContact} from 'react-icons/gr';
 import{RiServiceLine} from 'react-icons/ri';
 import{BsChatText,BsInfoCircle} from 'react-icons/bs';
+import{useState} from 'react';
 
 export default function Nav() {
+  const [activeNav, setActiveNAv] = useState('#');
   return (
     <nav>
-      <a href="#"><BiHomeCircle/></a>
-      <a href="#about"><BsInfoCircle/></a>
-      <a href="#experience"><BiBookOpen/></a>
-      <a href="#services"><RiServiceLine/></a>
-      <a href="#contact"><BsChatText/></a>
+      <a 
+        href="#" 
+        className={activeNav === '#' ? 'active' : ''}
+        onClick={() => setActiveNAv('#')}>
+        <BiHomeCircle/>
+      </a>
+      <a 
+        href="#about" 
+        className={activeNav === '#about' ? 'active' : ''}
+        onClick={() => setActiveNAv('#about')}>
+        <BsInfoCircle/>
+      </a>
+      <a 
+        href="#experience" 
+        className={activeNav === '#experience' ? 'active' : ''}
+        onClick={() => setActiveNAv('#experience')}>
+        <BiBookOpen/>
+      </a>
+      <a 
+        href="#services" 
+        className={activeNav === '#services' ? 'active' : ''}
+        onClick={() => setActiveNAv('#services')}>
+        <RiServiceLine/>
+      </a>
+      <a 
+        href="#contact"
+        className={activeNav === '#contact' ? 'active' : ''}
+        onClick={() => setActiveNAv('#contact')}>
+        <BsChatText/>
+      </a>
     </nav>
   )
 }
