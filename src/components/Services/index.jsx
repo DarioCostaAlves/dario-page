a649baeca5056252465c6c886ad06de57c25c5ab
DarioCostaAlves <dario.c.alves22@gmail.com>
2022-08-23 16:39:43 +0100

    services component

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -1,6 +1,96 @@
 import './styles.css';
+import {RiCheckDoubleLine} from 'react-icons/ri';
 export default function Services() {
   return (
-    <section id="services">Services</section>
+    <section id="services">
+      <h5>My offers</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>Mobile Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <RiCheckDoubleLine className="service__list-icon"/>
+              <p>Lorem, ipsum dolor.</p>
+            </li>
+            <li>
+              <RiCheckDoubleLine className="service__list-icon"/>
+              <p>Lorem, ipsum dolor.</p>
+            </li>
+            <li>
+              <RiCheckDoubleLine className="service__list-icon"/>
+              <p>Lorem, ipsum dolor.</p>
+            </li>
+            <li>
+              <RiCheckDoubleLine className="service__list-icon"/>
+              <p>Lorem, ipsum dolor.</p>
+            </li>
+            <li>
+              <RiCheckDoubleLine className="service__list-icon"/>
+              <p>Lorem, ipsum dolor.</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of Mobile Development */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <RiCheckDoubleLine className="service__list-icon"/>
+              <p>Lorem, ipsum dolor.</p>
+            </li>
+            <li>
+              <RiCheckDoubleLine className="service__list-icon"/>
+              <p>Lorem, ipsum dolor.</p>
+            </li>
+            <li>
+              <RiCheckDoubleLine className="service__list-icon"/>
+              <p>Lorem, ipsum dolor.</p>
+            </li>
+            <li>
+              <RiCheckDoubleLine className="service__list-icon"/>
+              <p>Lorem, ipsum dolor.</p>
+            </li>
+            <li>
+              <RiCheckDoubleLine className="service__list-icon"/>
+              <p>Lorem, ipsum dolor.</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of Web Development */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <RiCheckDoubleLine className="service__list-icon"/>
+              <p>Lorem, ipsum dolor.</p>
+            </li>
+            <li>
+              <RiCheckDoubleLine className="service__list-icon"/>
+              <p>Lorem, ipsum dolor.</p>
+            </li>
+            <li>
+              <RiCheckDoubleLine className="service__list-icon"/>
+              <p>Lorem, ipsum dolor.</p>
+            </li>
+            <li>
+              <RiCheckDoubleLine className="service__list-icon"/>
+              <p>Lorem, ipsum dolor.</p>
+            </li>
+            <li>
+              <RiCheckDoubleLine className="service__list-icon"/>
+              <p>Lorem, ipsum dolor.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
   )
 }
