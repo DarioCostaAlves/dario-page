9a0d6b244860e82825eb8b57901dc0fc78621d4d
DarioCostaAlves <dario.c.alves22@gmail.com>
2022-08-26 23:34:50 +0100

    contact component

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,6 +1,40 @@
 import './styles.css';
+import{MdOutlineMailOutline} from 'react-icons/md';
+import{BsLinkedin, BsWhatsapp} from 'react-icons/bs';
 export default function Contact() {
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">      
+      <h5>Get In Touch</h5>
+      <h2>Contact</h2>
+      
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMailOutline className="contact__option-icon"/>
+            <h4>Email</h4>
+            <h5>dario.c.alves22@gmail.com</h5>
+            <a href="mailto:dario.c.alves22@gmail.com" target="_blank">Send a message</a>            
+          </article>
+          <article className="contact__option">
+            <BsLinkedin className="contact__option-icon"/>
+            <h4>Linkedin</h4>
+            <h5>dariocoding</h5>
+            <a href="https://www.linkedin.com/in/dariocoding/" target="_blank">Send a message</a>            
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon"/>
+            <h4>WhatsApp</h4>
+            <h5>+351966514109</h5>
+            <a href="https://wa.me/+351966514109?text=Hi Dário" target="_blank">Send a message</a>            
+          </article>
+        </div>
+        <form action="">
+          <input type="text" name='name' placeholder='Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email' required/>
+          <textarea name="message" placeholder='Message' rows="8" required></textarea>
+          <button type="submit" className="btn btn-primary">Send</button>
+        </form>
+      </div>
+    </section>
   )
 }
