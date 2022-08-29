c9bb8ee1095624f22ec063691a77cd00ce69a0a2
DarioCostaAlves <dario.c.alves22@gmail.com>
2022-08-29 14:31:46 +0100

    typewritter effect and main pic for small width

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,12 +2,22 @@ import './styles.css';
 import CTA from './CTA';
 import ME from '../../assets/images/me.png';
 import HeaderSocials from './HeaderSocials';
+import Typewriter from 'typewriter-effect';
 export default function Header() {
   return (
     <header>
       <div className="container header__container">
         <h5>Hey, I'm</h5>
-        <h1>Dário Alves</h1>
+        <h1>
+          <Typewriter
+            options={{
+              strings: ['Dário Alves', 'Dário Costa Alves'],
+              autoStart: true,
+              loop: true,
+              delay: 200,
+            }}
+          />
+        </h1>
         <h5 className="text-light">Mobile Developer</h5>
         <CTA />
         <HeaderSocials />
